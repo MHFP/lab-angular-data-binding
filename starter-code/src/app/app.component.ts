1f61c29433836f04242dc025dd35f53a6240e955
MHFP <markus.hifp@gmail.com>
2017-09-28 07:43:41 +0200

    third iteration done

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import foods from './foods';
+
+class Food{name:string}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit{
+  searchTerm:string;
+  foods:Object[];
+
+  ngOnInit() {
+    this.foods = foods;
+  }
+
+  handleNewFood(food) {
+    this.foods.push(food);
+  }
+
 }
---
+++ starter-code/src/app/new-food-form/new-food-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-new-food-form',
+  templateUrl: './new-food-form.component.html',
+  styleUrls: ['./new-food-form.component.css']
+})
+export class NewFoodFormComponent implements OnInit {
+  @Output() onNewFood = new EventEmitter<Object>();
+  newFood: Object = {}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  handleSubmitClick(){
+    console.log(this.newFood);
+    this.onNewFood.emit(this.newFood);
+    this.newFood = {};
+  }
+}
